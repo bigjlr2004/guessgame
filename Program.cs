6fa697ba670fbc2fab0e521ae68b9421e329dfe8
John Roberts <bigjlr2004@yahoo.com>
2023-02-27 13:22:49 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace guessgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the secret number please.");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
